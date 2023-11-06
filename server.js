79f93c13e5e5552f1561767849d7f94f636e66f0
Mahajan Dhiraj <mahajandhiru001@gmail.com>
2023-11-06 22:05:49 +0530

    cors error fix

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://chat-app-client-theta-sepia.vercel.app/",
+    origin: "https://chat-app-client-theta-sepia.vercel.app",
   },
 });
 
